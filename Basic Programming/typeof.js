f7c409d4fb80f4ddad1cf814a54a79c50550ac15
Subhash Satyarthi <120436794+subhashprogrammer@users.noreply.github.com>
2025-07-01 09:59:49 +0530

    checked advance type of

--- Basic Programming/typeof.js
+++ Basic Programming/typeof.js
@@ -1,3 +1,12 @@
+const a = new Set()
+const b = new Map()
+const c = new Promise((resolve,reject)=>{
+    resolve('success')
+})
+const d = new Object()
+
+
+
 console.log(typeof(null))
 console.log(typeof(undefined))
 console.log(typeof('1'))
@@ -10,3 +19,9 @@ console.log(typeof(Set))
 console.log(typeof(Map))
 console.log(typeof(Promise))
 console.log(typeof([]))
+console.log(typeof(a))
+console.log(typeof(b))
+console.log(typeof(c))
+console.log(typeof(d))
+
+
