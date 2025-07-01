32af591aefbd2d249527c43c5a7e39163facfc45
Subhash Satyarthi <120436794+subhashprogrammer@users.noreply.github.com>
2025-07-01 09:49:02 +0530

    checked different types of coercion

---
+++ Basic Programming/coercion.js
@@ -0,0 +1,45 @@
+// Numbers with other types
+
+console.log(1 + 1); 
+console.log(1 + "1"); 
+console.log(1 + true); 
+console.log(1 + false); 
+console.log(1 + null); 
+console.log(1 + undefined);
+
+// string with other types  
+
+console.log("1" + 1); 
+console.log("1" + true);
+console.log("1" + null);
+console.log("1" + undefined);
+
+
+// boolean with other types
+
+console.log(true + true); 
+console.log(true + false);
+console.log(false + false);
+console.log(true + null); 
+console.log(true + undefined);
+console.log(true + "1");
+
+
+// null with other types
+
+console.log(null + 1);
+console.log(null + "1"); 
+console.log(null + true);
+console.log(null + undefined); 
+
+// undefined with other types
+
+console.log(undefined + 1); 
+console.log(undefined + "1"); 
+console.log(undefined + true);  
+console.log(undefined + null); 
+console.log(undefined + undefined); 
+
+
+
+
