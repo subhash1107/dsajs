e0fe01191b5946344c22525d6869af34f29a03fc
Subhash Satyarthi <120436794+subhashprogrammer@users.noreply.github.com>
2025-07-01 10:25:13 +0530

    find smallest and largest

--- Basic Programming/loop.js
+++ Basic Programming/loop.js
@@ -1,14 +1,59 @@
 // searching for an element in Array
 
-function searchEl (arr,el){
-   for(let i =0;i<arr.length;i++){
-    if(arr[i]===el){
-        return 1
+function searchEl(arr, el) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === el) {
+      return 1;
     }
-   }
+  }
 
-   return -1
+  return -1;
 }
 
-console.log(searchEl([2,6,7,'fg'],'fg'))
-console.log(searchEl([2,6,7,'fg'],'f'))
+console.log(searchEl([2, 6, 7, "fg"], "fg"));
+console.log(searchEl([2, 6, 7, "fg"], "f"));
+
+// count negative number in Array
+
+function countNegative(arr) {
+  let count = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < 0) {
+      count++;
+    }
+  }
+
+  return count
+}
+
+console.log(countNegative([2,8,2,8,-5,-6,4,-5,-3]))
+
+// find the largest number in the Array
+
+function findLargest (arr){
+    let largest = 0
+    for(let i=0;i<arr.length;i++){
+      if(arr[i]>largest){
+        largest = arr[i]
+      }
+    }
+
+    return largest
+}
+
+
+console.log('largest ',findLargest([2,5,4,2,65,5,6,]))
+
+// smallest Number
+
+function findSmallest(arr){
+    let sm = Infinity;
+    for(i in arr){
+        if(arr[i]<sm){
+            sm = arr[i]
+        }
+    }
+
+    return sm
+}
+console.log(findSmallest([5,6,5,85,8,2,3,55,9]))
--- Basic Programming/typeof.js
+++ Basic Programming/typeof.js
@@ -23,5 +23,6 @@ console.log(typeof(a))
 console.log(typeof(b))
 console.log(typeof(c))
 console.log(typeof(d))
+console.log(typeof(Infinity))
 
 
