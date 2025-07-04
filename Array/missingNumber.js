485eabd08ef92a3d6e0e229ab5c2a029c04f49a2
Subhash Satyarthi <120436794+subhashprogrammer@users.noreply.github.com>
2025-07-04 10:49:14 +0530

    learn use of xor operator in finding missing number and sum approach

---
+++ Array/missingNumber.js
@@ -0,0 +1,70 @@
+// # Missing Number
+
+// Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
+// Example 1:
+// Input: nums = [3,0,1]
+// Output: 2
+// Explanation:
+// n = 3 since there are 3 numbers, so all numbers are in the range [0,3]. 2 is the missing number in the range since it does not appear in nums
+// Example 2:
+// Input: nums = [0,1]
+// Output: 2
+// Explanation:
+// 2 since there are 2 numbers, so all numbers are in the range [0,2]. 2 is the missing number in the range since it does not appear in nums.
+// Example 3:
+// Input: nums = [9,6,4,2,3,5,7,0,1]
+// Output: 8
+// Explanation:
+// n = 9 since there are 9 numbers, so all numbers are in the range [0,9]. 8 is the missing number in the range since it does not appear in nums
+
+
+// 1. THIS SOLUTION IS OK BUT TAKING O(n2) time 
+
+
+// var missingNumber = function(nums) {
+//     for(let i = 0; i<=nums.length;i++){
+//         if(!nums.includes(i)){
+//             return i
+//         }
+//     }
+// };
+
+// using reduce
+
+// function missingNumber(nums){
+//     let n = nums.length
+//     let expectedSum = n*(n+1)/2
+//     let actualSum = nums.reduce((acc,current)=>acc+=current)
+//     return expectedSum - actualSum
+// }
+
+
+// leetcode style most preferred
+
+// function missingNumber(nums){
+//    const n = nums.length
+//    let expectedSum = n*(n+1)/2
+//    let actualSum = 0
+//    for(let i = 0; i<n ; i++){
+//         actualSum+=nums[i]
+//    }
+//    return expectedSum-actualSum
+// }
+
+
+// using xor operator
+
+function missingNumber(nums){
+    let xor = 0
+    for(let i = 0; i<= nums.length; i++){
+        xor ^= i
+    }
+    for(let num of nums){
+        xor ^= num
+    }
+    return xor
+}
+
+console.log(missingNumber([3,0,1]))
+console.log(missingNumber([0,1]))
+console.log(missingNumber([9,6,4,2,3,5,7,0,1]))
