98eda77f100f39967a6f29a73e068a7a2daeb3ea
Subhash Satyarthi <120436794+subhashprogrammer@users.noreply.github.com>
2025-07-07 08:50:23 +0530

    bubble and selection sort

---
+++ SearchingSorting/bubbleSort.js
@@ -0,0 +1,17 @@
+function bubbleSort(arr) {
+    let n = arr.length;
+    for (let i = 0; i < n - 1; i++) {
+        let swapped = false;
+        for (let j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+                swapped = true;
+            }
+        }
+        if (!swapped) break;
+    }
+    return arr;
+}
+
+
+console.log(bubbleSort([2,0,2,1,1,0]))
