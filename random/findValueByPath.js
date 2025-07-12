5bbd47d2d42c2c394c49a4d08fb23522e2340ee5
Subhash Satyarthi <120436794+subhashprogrammer@users.noreply.github.com>
2025-07-12 09:13:48 +0530

    find value by path

--- random/findValueByPath.js
+++ random/findValueByPath.js
@@ -1,6 +1,25 @@
+// function getResultByPath(path, obj) {
+//   //write your implementation here
+//   return path.split('.').reduce((acc,curr)=>acc?.[curr],obj)
+// }
+// function getResultByPath(path, obj) {
+//   //write your implementation here
+//   let current = obj
+//   for(let key of path.split('.')){
+//     current = current[key]
+//   }
+//   return current
+// }
 function getResultByPath(path, obj) {
-  //write your implementation here
-  return path.split('.').reduce((acc,curr)=>acc?.[curr],obj)
+  const normalizedPath = path.replace(/\[(\d+)]/g, '.$1')
+  console.log(normalizedPath)
+  const keys = normalizedPath.split('.')
+  let result = obj
+  for (let key of keys) {
+    if (result === undefined || result === null) break
+    result=result[key]
+  }
+  return result
 }
 const path = "data.results.status";
 const obj = {
