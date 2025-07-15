4f057cd6ffc9490da6e8198a0e36dfed2642dcaa
Subhash Satyarthi <120436794+subhashprogrammer@users.noreply.github.com>
2025-07-15 14:32:53 +0530

    findlargest

--- random/findLargestPossible.js
+++ random/findLargestPossible.js
@@ -1,18 +1,8 @@
-function formLargestNumber(arr) {
-  const newArr = []
-  for(let i = 0; i<arr.length; i++){
-    if(arr[i]>=0&&arr[i]<=9){
-        newArr.push(arr[i].toString())
-    } else if(arr[i]>9){
-        for(let j of arr[i].toString()){
-            newArr.push(j)
-        }
-    }
-  }
+function formLargestNumber(arr) {  
+  const newArr = arr.map(item=>item.toString()).sort((a,b)=>(b+a).localeCompare(a+b))
+  if(newArr[0]==='0') return '0'
+  return newArr.join('')
 
-  
-  
-  return newArr.sort((a,b)=>b-a).join('')
 }
 const input = [3, 30, 34, 5, 9];
 console.log(formLargestNumber(input));
